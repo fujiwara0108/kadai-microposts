9da84172a9c30110a5bf99ef9d5f6cdfdd1831b0
motoki.fujiwara <h0108bfddrr@gmail.com>
2020-07-14 13:19:01 +0000

    post

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,5 +1,9 @@
 class ToppagesController < ApplicationController
   #それぞれのアクションの最後に'render:自身のアクション名が呼びだされてる
   def index
+    if logged_in?
+      @micropost = current_user.microposts.build #form_with(model: @micropost)として使用
+      @microposts = current_user.microposts.order(id: :desc).page(params[:page])
+    end
   end
 end
